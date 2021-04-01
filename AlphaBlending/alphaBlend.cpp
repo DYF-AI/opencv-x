bd9dabe7b9cc122349aced797e99d43f443d74c6
DYF-AI <dongyongfei36@gmail.com>
2021-04-01 23:40:28 +0800

    alphaBlend

--- AlphaBlending/alphaBlend.cpp
+++ AlphaBlending/alphaBlend.cpp
@@ -1,15 +1,13 @@
 
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-using namespace std;
 
 // 使用cv :: multiply和添加功能进行Alpha混合 
 cv::Mat& blend(cv::Mat& alpha, cv::Mat& foreground, cv::Mat& background, cv::Mat& outImage)
 {
     cv::Mat fore, back;
     cv::multiply(alpha, foreground, fore);
-    cv::multiply(Scalar::all(1.0)-alpha, background, back);
+    cv::multiply(cv::Scalar::all(1.0)-alpha, background, back);
     cv::add(fore, back, outImage);
     
     return outImage;
@@ -38,59 +36,57 @@ cv::Mat& alphaBlendDirectAccess(cv::Mat& alpha, cv::Mat& foreground, cv::Mat& ba
 
 int main(int argc, char** argv)
 {
-    
-    // Read in the png foreground asset file that contains both rgb and alpha inforcv::mation
-    cv::Mat foreGroundImage = imread("../images/foreGroundAssetLarge.png", -1);
+    // 读取前景图片
+    cv::Mat foreGroundImage = cv::imread("../images/foreGroundAssetLarge.png", -1);
     cv::Mat bgra[4];
-    split(foreGroundImage, bgra);//split png foreground
+    //通道分离
+    cv::split(foreGroundImage, bgra);
     
-    // Save the foregroung RGB content into a single cv::Mat
-    vector<cv::Mat> foregroundChannels;
+    // 将RGB内容保存到单个cv :: Mat中 
+    std::vector<cv::Mat> foregroundChannels;
     foregroundChannels.push_back(bgra[0]);
     foregroundChannels.push_back(bgra[1]);
     foregroundChannels.push_back(bgra[2]);
     cv::Mat foreground = cv::Mat::zeros(foreGroundImage.size(), CV_8UC3);
     merge(foregroundChannels, foreground);
     
-    // Save the alpha inforcv::mation into a single cv::Mat
-    vector<cv::Mat> alphaChannels;
+    // 将Alpha信息保存到单个cv :: Mat中 
+    std::vector<cv::Mat> alphaChannels;
     alphaChannels.push_back(bgra[3]);
     alphaChannels.push_back(bgra[3]);
     alphaChannels.push_back(bgra[3]);
     cv::Mat alpha = cv::Mat::zeros(foreGroundImage.size(), CV_8UC3);
     merge(alphaChannels, alpha);
    
-    // Read background image
-    cv::Mat background = imread("../images/backGroundLarge.jpg");
+    cv::Mat background = cv::imread("../images/backGroundLarge.jpg");
     
-    // Convert cv::Mat to float data type
+    // 将cv :: Mat转换为float数据类型 
     foreground.convertTo(foreground, CV_32FC3);
     background.convertTo(background, CV_32FC3);
     alpha.convertTo(alpha, CV_32FC3, 1.0/255); // keeps the alpha values betwen 0 and 1
 
-    // Number of iterations to average the performane over
     int numOfIterations = 1; //1000;
     
-    // Alpha blending using functions cv::multiply and add
+    // 使用函数cv :: multiply进行Alpha混合并相加 
     cv::Mat outImage= cv::Mat::zeros(foreground.size(), foreground.type());
-    double t = (double)getTickCount();
+    double t = (double)cv::getTickCount();
     for (int i=0; i<numOfIterations; i++) {
         outImage = blend(alpha, foreground, background, outImage);
     }
-    t = ((double)getTickCount() - t)/getTickFrequency();
-    cout << "Time for alpha blending using cv::multiply & add function : " << t*1000/numOfIterations << " milliseconds" << endl;
+    t = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
+    std::cout << "Time for alpha blending using cv::multiply & add function : " << t*1000/numOfIterations << " milliseconds" << std::endl;
 
-    // Alpha blending using direct cv::Mat access with for loop
+    // 使用直接cv :: Mat访问和for循环进行Alpha混合 
     outImage = cv::Mat::zeros(foreground.size(), foreground.type());
-    t = (double)getTickCount();
+    t = (double)cv::getTickCount();
     for (int i=0; i<numOfIterations; i++) {
         outImage = alphaBlendDirectAccess(alpha, foreground, background, outImage);
     }
-    t = ((double)getTickCount() - t)/getTickFrequency();
-    cout << "Time for alpha blending using alphaBlendDirectAccess : " << t*1000/numOfIterations << " milliseconds" << endl;
+    t = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
+    std::cout << "Time for alpha blending using alphaBlendDirectAccess : " << t*1000/numOfIterations << " milliseconds" << std::endl;
 
-    imshow("alpha blended image", outImage/255);
-    waitKey(0);
+    cv::imshow("alpha blended image", outImage/255);
+    cv::waitKey(0);
     
     return 0;
 }
